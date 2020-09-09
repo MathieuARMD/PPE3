52606d0aa70ce36fa90a6f794597c2031c8fd47e
Taugry <teo.augry@limayrac.fr>
2020-09-09 11:33:51 +0200

    ajout du folder css et de l'index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--<link rel="stylesheet" href="css\main.css">-->
+    <title>Accueil</title>
+</head>
+<body>     
+    <p>Bienvenue dans la FAQ de la ligue de football</p>
+    <p>Pour continuer, vous devez vous connecter ou vous inscrire</p>
+    
+</body>
+</html>
