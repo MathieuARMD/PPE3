97992878bb1e220ba208f9fff881e8daa6c107f8
Taugry <teo.augry@limayrac.fr>
2020-09-09 11:46:28 +0200

    ajout de css et de ficher pour l acceuil

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!--<link rel="stylesheet" href="css\main.css">-->
-    <title>Accueil</title>
+    <link rel="stylesheet" href="css\main.css">
+    <title>Accueil FREDI</title>
 </head>
-<body>     
-    <p>Bienvenue dans la FAQ de la ligue de football</p>
+<body>
+    <?php include 'top.php';?>
+    <br>
+        
+    <p>Bienvenue sur FREDI</p>
     <p>Pour continuer, vous devez vous connecter ou vous inscrire</p>
     
 </body>
