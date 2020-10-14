7920e296d71dc55adba74c814054242f4d6d4c61
AUGRY <AT359211>
2020-10-14 14:41:14 +0200

    ajout liste dynamique sur les modif users

--- modifier.php
+++ modifier.php
@@ -142,10 +142,31 @@ switch ($modif) { //si pas de session -> echo erreur
   echo "</tr>"; 
 echo "</table>";
 ?><!--From pour modifier l'utilisateur en question -->
+<?php
+$sql = "SELECT email_util FROM utilisateur"; // requete sql
+$dsn = 'mysql:host=localhost;dbname=fredi;charset=UTF8'; 
+$user = 'root';
+$password = '';
+try {
+$dbh = new PDO($dsn, $user, $password);
+$sth = $dbh->prepare($sql);
+$sth->execute(); 
+$raws = $sth->fetchALL(PDO::FETCH_ASSOC);
+} catch (PDOException $ex) {
+die("Erreur lors de la requête SQL : ".$ex->getMessage());
+}
+?>
 <br>
  <form action="modifier.php" method="post">
-  <label for="email">E-Mail :</label><br>
-  <input type="email" id="email" name="email" required><br><br>
+  <select name="email" id="email" required>
+    <?php
+        foreach($raws as $raw){
+        foreach($raw as $value){
+            echo "<option value='" .$value. "'>" .$value. "</option>";
+        }
+        }    
+    ?>
+    </select><br><br>
   <label for="nom">Nom :</label><br>
   <input type="text" id="nom" name="nom" required><br><br>
   <label for="prenom">Prenom :</label><br>
