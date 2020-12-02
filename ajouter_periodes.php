12eb2281ada23c1804a21362e50a6f83077e5ebd
AUGRY <AT359211>
2020-12-02 09:59:25 +0100

    modif

--- ajouter_periodes.php
+++ ajouter_periodes.php
@@ -50,34 +50,43 @@
           }else{
             $forfait = $_POST['forfait'];
           }
-          
-          $statut = 1;
-          $rows = $PeriodeDAO->findDisabled();
-              foreach($rows as $row){
-                $DByear = $row["annee_per"];
-                $DBstatut = $row["statut_per"];
-              } $res = $DByear - $date;
-              if($res >= 0){
-                echo "<br><br>"; 
-                echo "<p>Vous ne pouvez pas créer la période $date car l’année n’est pas valide</p>";                
-              }elseif($statut == $DBstatut){
-                echo "<br><br>"; 
-                echo "<p>Vous ne pouvez pas créer la période $date car une période active existe déjà</p>";  
-              }else{
-                $periode = new Periode(array(
-                  'annee'=>$date,
-                  'forfait'=>$forfait,
-                  'statut'=>$statut
-                ));
-                $count = $PeriodeDAO->insert($periode);
-                    if($count == 1){
-                      echo "<br><br>"; 
-                      echo "<p>La periode de $date a été créé dans l’application FREDI</p>";
-                    }else{
-                      echo "<br><br>"; 
-                      echo "<p>La periode de $date n'a pas été créé dans l’application FREDI</p>";                   
-                    }
-              }        
+          $statut = 0;
+
+          $raws = $PeriodeDAO->statutest();
+          foreach($raws as $raw){
+            $nb2=$raw['stat'];
+            $annee=$raw['annee_per'];
+          }
+          if($nb2 == 1){
+              echo"<br><p>La periode ".$annee. " est déjà activée veuillez la désactiver pour crée une nouvelle periode</p>";
+          }else{
+            $rows = $PeriodeDAO->findDisabled();
+                foreach($rows as $row){
+                  $DByear = $row["annee_per"];
+                  $DBstatut = $row["statut_per"];
+                } $res = $DByear - $date;
+                if($res >= 0){
+                  echo "<br><br>"; 
+                  echo "<p>Vous ne pouvez pas créer la période $date car l’année n’est pas valide</p>";                
+                }elseif($statut == $DBstatut){
+                  echo "<br><br>"; 
+                  echo "<p>Vous ne pouvez pas créer la période $date car une période active existe déjà</p>";  
+                }else{
+                  $periode = new Periode(array(
+                    'annee'=>$date,
+                    'forfait'=>$forfait,
+                    'statut'=>$statut
+                  ));
+                  $count = $PeriodeDAO->insert($periode);
+                      if($count == 1){
+                        echo "<br><br>"; 
+                        echo "<p>La periode de $date a été créé dans l’application FREDI</p>";
+                      }else{
+                        echo "<br><br>"; 
+                        echo "<p>La periode de $date n'a pas été créé dans l’application FREDI</p>";                   
+                      }
+                }
+            }        
         }
 ?>
 </form>
